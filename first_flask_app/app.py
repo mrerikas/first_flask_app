fb7b38975b39a2891e18cc69c27253d8180d3ff5
mrerikas <erikas.mise@gmail.com>
2021-02-15 16:19:57 +0200

    added main.html, will be main layout template

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/<string:name>")
+def hello(name):
+    return f"<h1>Hello, {name}<h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
